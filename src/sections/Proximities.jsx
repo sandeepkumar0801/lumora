08dba83e14713a46b6416b8100e84120267c9590
HevenDev <hevendevofficial@gmail.com>
2025-02-28 11:36:42 +0530

    Proximities content updated

--- src/sections/Proximities.jsx
+++ src/sections/Proximities.jsx
@@ -11,8 +11,8 @@ const Proximities = () => {
             <h3 className="text-center sm:text-lg text-sm">Delhi</h3>
           </div>
           <div className="py-4 border-greenTheme border-solid border-t">
-            <p className="text-center sm:text-lg text-sm">2.5 Hours away from</p>
-            <h3 className="text-center sm:text-lg text-sm">Delhi</h3>
+            <p className="text-center sm:text-lg text-sm">1 Hour away from</p>
+            <h3 className="text-center sm:text-lg text-sm">Dehradun Airport</h3>
           </div>
         </div>
         <div className="lg:w-[24%] w-[50%] md:border-b lg:border-b-0 border-b-1 flex flex-col justify-center lg:border-r border-greenTheme border-solid border-r-1">
@@ -22,22 +22,22 @@ const Proximities = () => {
             <h3 className="text-center sm:text-lg text-sm">Expressway</h3>
           </div>
           <div className="py-4 border-greenTheme border-solid border-t">
-            <p className="text-center sm:text-lg text-sm">Bang on</p>
-            <h3 className="text-center sm:text-lg text-sm">Delhi-Dehradun</h3>
-            <h3 className="text-center sm:text-lg text-sm">Expressway</h3>
+            <p className="text-center sm:text-lg text-sm">2 Hours away from</p>
+            <h3 className="text-center sm:text-lg text-sm">AIMS</h3>
+            <h3 className="text-center sm:text-lg text-sm">Rishikesh</h3>
           </div>
         </div>
-        <div className="lg:w-[24%] w-[50%]  flex flex-col justify-center border-r border-t border-t-1 lg:border-t-0 border-greenTheme border-solid border-r-1">
+        <div className="lg:w-[24%] w-[50%]  flex flex-col justify-center border-t border-t-1 lg:border-t-0 ">
           <div className="py-4">
             <p className="text-center sm:text-lg text-sm">2 Hours away from</p>
             <h3 className="text-center sm:text-lg text-sm">Saharanpur</h3>
           </div>
           <div className="py-4 border-greenTheme border-solid border-t">
             <p className="text-center sm:text-lg text-sm">2 Hours away from</p>
-            <h3 className="text-center sm:text-lg text-sm">Saharanpur</h3>
+            <h3 className="text-center sm:text-lg text-sm">Teri Lake</h3>
           </div>
         </div>
-        <div className="lg:w-[24%] w-[50%] border-t border-t-1 border-greenTheme border-solid lg:border-t-0 flex flex-col justify-center">
+        {/* <div className="lg:w-[24%] w-[50%] border-t border-t-1 border-greenTheme border-solid lg:border-t-0 flex flex-col justify-center">
           <div className="py-4">
             <p className="text-center sm:text-lg text-sm">2.5 Hours away from</p>
             <h3 className="text-center sm:text-lg text-sm">Delhi</h3>
@@ -46,7 +46,7 @@ const Proximities = () => {
             <p className="text-center sm:text-lg text-sm">2.5 Hours away from</p>
             <h3 className="text-center sm:text-lg text-sm">Delhi</h3>
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   )
