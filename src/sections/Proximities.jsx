eca05d93b2d515d817d225b34a08182f1c853f65
HevenDev <hevendevofficial@gmail.com>
2025-02-28 17:56:06 +0530

    Proximities content updated

--- src/sections/Proximities.jsx
+++ src/sections/Proximities.jsx
@@ -7,12 +7,12 @@ const Proximities = () => {
       <div className="flex flex-wrap justify-center items-stretch">
         <div className="lg:w-[24%] w-[50%]  flex flex-col justify-center border-r border-greenTheme border-solid border-r-1 ">
           <div className="py-4">
-            <p className="text-center sm:text-lg text-sm">2.5 Hours away from</p>
+            <p className="text-center sm:text-lg text-sm">3.5 Hours away from</p>
             <h3 className="text-center sm:text-lg text-sm">Delhi</h3>
           </div>
           <div className="py-4 border-greenTheme border-solid border-t">
-            <p className="text-center sm:text-lg text-sm">1 Hour away from</p>
-            <h3 className="text-center sm:text-lg text-sm">Dehradun Airport</h3>
+            <p className="text-center sm:text-lg text-sm">15 Mins away from</p>
+            <h3 className="text-center sm:text-lg text-sm">Shakumbhari Devi Temple</h3>
           </div>
         </div>
         <div className="lg:w-[24%] w-[50%] md:border-b lg:border-b-0 border-b-1 flex flex-col justify-center lg:border-r border-greenTheme border-solid border-r-1">
@@ -30,16 +30,16 @@ const Proximities = () => {
         <div className="lg:w-[24%] w-[50%]  flex flex-col border-greenTheme border-solid border-r-1 border-r justify-center border-t border-t-1 lg:border-t-0 ">
           <div className="py-4">
             <p className="text-center sm:text-lg text-sm">2 Hours away from</p>
-            <h3 className="text-center sm:text-lg text-sm">Saharanpur</h3>
+            <h3 className="text-center sm:text-lg text-sm">Rishikesh</h3>
           </div>
           <div className="py-4 border-greenTheme border-solid border-t">
-            <p className="text-center sm:text-lg text-sm">2 Hours away from</p>
-            <h3 className="text-center sm:text-lg text-sm">Teri Lake</h3>
+            <p className="text-center sm:text-lg text-sm">3 Hours away from</p>
+            <h3 className="text-center sm:text-lg text-sm">Tehri Lake</h3>
           </div>
         </div>
         <div className="lg:w-[24%] w-[50%] border-t border-t-1 border-greenTheme border-solid lg:border-t-0 flex flex-col justify-center">
           <div className="py-4">
-            <p className="text-center sm:text-lg text-sm">4 Hours away from</p>
+            <p className="text-center sm:text-lg text-sm">3 Hours away from</p>
             <h3 className="text-center sm:text-lg text-sm">Sonipat</h3>
           </div>
           <div className="py-4 border-greenTheme border-solid border-t">
