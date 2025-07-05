5f86b4d74fc2a386e28984ab0e657788f8ea4921
Sandeep Kumar <69627362+sandeepkumar0801@users.noreply.github.com>
2025-07-06 03:07:26 +0530

    update for build

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['images.unsplash.com', 'plus.unsplash.com', 'source.unsplash.com'],
-  },
-};
-
-export default nextConfig;
